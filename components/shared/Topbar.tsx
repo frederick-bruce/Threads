a72ce94c0387d0c0a9a9525a8daa6dcda9730b04
frederick-bruce <frederick.bruce.dev@gmail.com>
2023-08-05 18:05:51 -0400

    Update Topbar.tsx
    organization switcher fix

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,7 +7,7 @@ function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <Image src="assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
@@ -31,7 +31,7 @@ function Topbar() {
           appearance={{
             baseTheme: dark,
             elements: {
-              organizationSwitcherTrigger: "p-2 px-4",
+              organizationSwitcherTrigger: "py-2 px-4",
             },
           }}
         />
