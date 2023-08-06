e6a9a90db6a53a9dd4c7148267d43c6b0ad8afbb
frederick-bruce <frederick.bruce.dev@gmail.com>
2023-08-06 13:59:47 -0400

    thread
    Thread Card update
    feat: Delete Thread

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const result = await fetchPosts(1, 30);
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
+  if (!user) return null;
 
-  console.log(result);
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
@@ -15,23 +28,25 @@ export default async function Home() {
         {result.posts.length === 0 ? (
           <p className="no-result">No threads found</p>
         ) : (
-            <>
-              {result.posts.map((post) => (
-                <ThreadCard
-                  key={post._id}
-                  id={post._id}
-                  currentUserId={user?.id || ""}
-                  parentId={post.parentId}
-                  content={post.text}
-                  author={post.author}
-                  community={post.community}
-                  createdAt={post.createdAt}
-                  comments={post.children}
-                />
-              ))}
-            </>
-          )}
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
       </section>
     </>
   );
 }
+
+export default Home;
